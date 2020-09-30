bbdf3d64e2815f0ccaee859729262987d644ffca
Olli-Pekka Nikka <s0niol00@students.osao.fi>
2020-09-30 10:53:05 +0300

    added nav

--- testaus/app.js
+++ testaus/app.js
@@ -8,20 +8,19 @@ const btn2 = document.getElementById("btn-2");
 const btn3 = document.getElementById("btn-3");
 const btn4 = document.getElementById("btn-4");
 
+const dateElement = document.getElementById("date");
 const date = new Date();
+dateElement.innerHTML = date;
 
 btn1.addEventListener("click", () => {
-  testPara1.innerHTML = "Olli-Pekka Nikka"
+  testPara1.innerHTML = "Olli-Pekka Nikka";
 })
 btn2.addEventListener("click", () => {
-  testPara2.innerHTML = "Kotkantie 2, Oulu"
+  testPara2.innerHTML = "Kotkantie 2, Oulu";
 })
 btn3.addEventListener("click", () => {
   testPara3.innerHTML = "Tänään on: " + date;
 })
 btn4.addEventListener("click", () => {
-  alert("Tänään on: " + date)
+  alert("Tänään on: " + date);
 })
-
-
-
