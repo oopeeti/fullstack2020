707fc16d4bd9e4ac272c19ba2f54fa9b2a7bb7c7
Olli-Pekka Nikka <ollipekkanikka@gmail.com>
2020-10-06 10:00:52 +0300

    Lisätty 6.10 tehtävä ja tyylejä

--- nettisivu/js/app.js
+++ nettisivu/js/app.js
@@ -27,5 +27,42 @@ resetBtn.addEventListener("click", () => {
   divTxt.style.backgroundColor = "";
 });
 
+// 6.10.2020 - Laskut
+
+const vastaus = document.getElementById("vastaus");
+const lasku1 = document.getElementById("lasku1-input"); // lasku 1 input
+const lasku2 = document.getElementById("lasku2-input"); // lasku 2 input
+const addBtn = document.getElementById("add-btn"); // + nappi
+const subBtn = document.getElementById("sub-btn"); // - nappi
+const divideBtn = document.getElementById("divide-btn"); // nappi
+const multiplyBtn = document.getElementById("multiply-btn"); // * nappi
+
+addBtn.addEventListener("click", () => {
+  const num1 = parseInt(lasku1.value);
+  const num2 = parseInt(lasku2.value);
+  vastaus.innerHTML = `Vastaus: ${num1} + ${num2} = ${num1 + num2}`
+});
+
+subBtn.addEventListener("click", () => {
+  const num1 = parseInt(lasku1.value);
+  const num2 = parseInt(lasku2.value);
+  vastaus.innerHTML = `Vastaus: ${num1} - ${num2} = ${num1 - num2}`
+});
+
+divideBtn.addEventListener("click", () => {
+  const num1 = parseInt(lasku1.value);
+  const num2 = parseInt(lasku2.value);
+  vastaus.innerHTML = `Vastaus: ${num1} / ${num2} = ${num1 / num2}`
+});
+
+multiplyBtn.addEventListener("click", () => {
+  const num1 = parseInt(lasku1.value);
+  const num2 = parseInt(lasku2.value);
+  vastaus.innerHTML = `Vastaus: ${num1} * ${num2} = ${num1 * num2}`
+});
+
+
+
+
 
 
